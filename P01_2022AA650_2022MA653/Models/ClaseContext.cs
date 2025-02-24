c15ff7933f790d41a5c9e19c49f0c5c4d1632652
Rod <oscar.aguilar1@catolica.edu.sv>
2025-02-24 17:06:04 -0600

    Add project files.

---
+++ P01_2022AA650_2022MA653/Models/ClaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace P01_2022AA650_2022MA653.Models
+{
+    public class ClaseContext: DbContext
+    {
+        public ClaseContext(DbContextOptions<ClaseContext> options) : base(options)
+        {
+
+        }
+/*
+        public DbSet<usuarios> Usuarios { get; set; }
+        public DbSet<comentarios> comentarios { get; set; }
+        public DbSet<roles> roles { get; set; }
+        public DbSet<calificaciones> calificaciones { get; set; }
+        public DbSet<publicaciones> publicaciones { get; set; }*/
+    }
+}
